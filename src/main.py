ed22443d9f70931c46bb6ed03fc68db91f51d1d4
panconlocro <rosamariarv10@gmail.com>
2026-04-24 18:47:30 -0400

    driver completo mas ejemplos y correccion de raiz del directorio para el main

--- src/main.py
+++ src/main.py
@@ -1,18 +1,28 @@
 import sys
+import os
+
+ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.insert(0, ROOT_DIR)
+
 from antlr4 import *
-from NetLangLexer import NetLangLexer
-from NetLangParser import NetLangParser
+from grammar.NetLangLexer import NetLangLexer
+from grammar.NetLangParser import NetLangParser
 
 def main():
+    if len(sys.argv) < 2:
+        print("Uso: python3 src/main.py <archivo.txt>")
+        sys.exit(1)
+
     input_stream = FileStream(sys.argv[1])
     lexer = NetLangLexer(input_stream)
     tokens = CommonTokenStream(lexer)
     parser = NetLangParser(tokens)
     tree = parser.program()
-    
+
     if parser.getNumberOfSyntaxErrors() == 0:
-        print("✓ Parse exitoso")
+        print("✓ Parse exitoso. No se encontraron errores sintácticos.")
+        print(tree.toStringTree(recog=parser))
     else:
-        print("✗ Errores encontrados")
+        print(f"✗ Se encontraron {parser.getNumberOfSyntaxErrors()} error(es) sintáctico(s).")
 
 main()
